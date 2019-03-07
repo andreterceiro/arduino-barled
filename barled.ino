024b3a646cbdd0c712cc6beddeeca16872263118
terceiro.com.br <andre@terceiro.com.br>
2019-03-07 00:50:51 -0300

    Update barled.ino

--- barled.ino
+++ barled.ino
@@ -1,11 +1,15 @@
 void setup() {
-    pinMode(2, OUTPUT);
+    pinMode(1, OUTPUT);
+    pinMode(2, OUTPUT);    
     pinMode(3, OUTPUT);
     pinMode(4, OUTPUT);
     pinMode(5, OUTPUT);
     pinMode(6, OUTPUT);        
     pinMode(7, OUTPUT);
- }
+    pinMode(8, OUTPUT);
+    pinMode(9, OUTPUT);        
+    pinMode(10, OUTPUT);
+}
 
 void loop() {
   digitalWrite(1, HIGH);
@@ -15,6 +19,9 @@ void loop() {
   digitalWrite(5, HIGH);
   digitalWrite(6, HIGH);  
   digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, HIGH);    
   delay(1000);
   
   digitalWrite(1, LOW);
@@ -24,5 +31,8 @@ void loop() {
   digitalWrite(5, LOW);
   digitalWrite(6, LOW);  
   digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);    
   delay(1000);
  }
